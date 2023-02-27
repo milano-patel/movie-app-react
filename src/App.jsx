fd2b8cf20eea279969bd2e089e560ebdcb46ce47
Milan_Patel <ptlmilan52@gmail.com>
2023-02-27 11:51:11 +0530

    First commit..

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ function App() {
   const API_URL =
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=_YOUR API KEY_&page=';
 
-  const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-
   const SEARCH_API =
     'https://api.themoviedb.org/3/search/movie?api_key=_YOUR API KEY_&query="';
 
